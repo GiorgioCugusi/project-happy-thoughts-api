c5140c4d6f858c3c4a1033d9155611bfe943e7e8
GG_Cugs <giorgio.cugusi@gmail.com>
2023-05-18 12:52:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,55 @@ app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
+const { Schema } = mongoose;
+const FruitOrVegetableSchema = new Schema({
+  name: {
+    // Most important one
+    type: String,
+    // Required true or false
+    required: true,
+    // Only a new name that is different than all the others is allowed
+    unique: true
+  },
+  description: {
+    type: String,
+    // set up max and min lenght
+    minlength: 4,
+    maxlength: 40,
+    // removes unnecessary white spaces from string
+    trim: true
+  },
+  createdAt: {
+    type: Date,
+    default: new Date()
+  },
+  kind: {
+    type: String,
+    // an array of all the allowed values
+    enum: ["fruit", "vegetables"]
+  }
+});
+
+
+const FruitOrVegetable = mongoose.model("FruitOrVegetable", FruitOrVegetableSchema)
+
+app.post("/fruit_or_vegetable", async (req, res) => {
+  const {kind, name, description} = req.body;
+  try{
+    const foodItem = await new FruitOrVegetable({kind, name, description}).save();
+    res.status(201).json({
+      success: true,
+      response: foodItem
+    });
+  } catch(e) {
+    res.status(400).json({
+      success: false,
+      response: e
+    });
+  }
+})
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
