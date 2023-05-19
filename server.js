3e9f80b80c03676154efc21ad21a48ff424f08f6
GG_Cugs <giorgio.cugusi@gmail.com>
2023-05-19 09:53:19 +0200

    backend done

--- server.js
+++ server.js
@@ -56,9 +56,111 @@ app.get("/thoughts", async (req, res) => {
   res.json(thoughts);
 });
 
+// Endpoint to post a thought:
+app.post('/thoughts', async (req, res) => {
+  // Retreieve information sent by the client to our API endpoint:
+  const { message, name } = req.body;
+  // Use our mongoose model to create the database entry:
+  const thought = new Thought({ message, name });
+  try {
+    const savedThought = await thought.save();
+    res.status(201).json(savedThought);
+  }
+  catch (err) {
+    res.status(400).json({message: 'Could not save thought', error: err.errors});
+  }
+});
+
+// Testing endpoint to find one thought
+app.get('/thoughts/:_id', async (req, res) => {
+  try {
+    const singleThought = await Thought.findById(req.params._id);
+    if (singleThought) {
+      res.status(200).json({
+        message: "Here is a thought",
+        success: true,
+        body: singleThought
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: "Faulty ID"
+        }
+      })
+    }
+  } catch(e) {
+    res.status(500).json({
+      success: false,
+      body: {
+        message: e
+      }
+    })
+  }
+});
+
+// Endpoint to like a single thought:
+app.post('/thoughts/:_id/like', async (req, res) => {
+    try {
+      const singleThought = await Thought.findById(req.params._id);
+      if (singleThought) {
+        singleThought.hearts += 1;
+        await singleThought.save();
+        res.status(200).json({
+          success: true,
+          message: "Thought successfully liked",
+          singleThought: singleThought
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          body: {
+            message: "Faulty ID"
+          }
+        })
+      }
+    } catch(e) {
+      res.status(500).json({
+        success: false,
+        body: {
+          message: "Server error",
+          error: e
+        }
+      })
+    }
+  });
+
+// Endpoint that deletes a thought:
+app.delete('/thoughts/:_id', async (req, res) => {
+  try {
+    const deletedThought = await Thought.findByIdAndDelete(req.params._id);
+    if (deletedThought) {
+      res.status(200).json({
+        success: true,
+        message: "Thought successfully deleted",
+        deletedThought: deletedThought
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: "Faulty ID"
+        }
+      });
+    }
+  } catch(e) {
+    res.status(500).json({
+      success: false,
+      body: {
+        message: "Server error",
+        error: e
+      }
+    });
+  }
+});
 
 
-app.post("/fruit_or_vegetable", async (req, res) => {
+/* app.post("/fruit_or_vegetable", async (req, res) => {
   const {kind, name, description} = req.body;
   try{
     const foodItem = await new FruitOrVegetable({kind, name, description}).save();
@@ -72,7 +174,7 @@ app.post("/fruit_or_vegetable", async (req, res) => {
       response: e
     });
   }
-})
+}) */
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
